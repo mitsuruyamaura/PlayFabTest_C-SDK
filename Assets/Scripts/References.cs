64ff857609416cb770a79cde17b4a29a2e00a392
mitsuru_yamaura <komayou326@gmail.com>
2021-11-03 16:42:26 +0900

    PlayFab C# SDK インストール。最新版ではエラーが出るため、エラー対応。 PlayFabLogin.cs 作成。Playfab に Title 登録。ログイン成功。

---
+++ Assets/Scripts/References.cs
@@ -0,0 +1,19 @@
+/// <summary>
+/// 参照先
+/// </summary>
+public class References {
+    // Azure PlayFab
+    //https://azure.microsoft.com/ja-jp/services/playfab/
+
+    // PlayFab 開発者サイト
+    //https://developer.playfab.com/ja-JP/sign-up
+
+    // PlayFab C# SDK
+    //https://aka.ms/playfabCsharpsdkdownload
+
+    // 最新版 1.101 PlayFabSettings.cs でのエラー対応
+    //https://xrdnk.hateblo.jp/entry/2021/09/19/220220
+    //①　PlayFabSettings.cs の 40行目に || !NET_STANDARD2_0、54行目に || !NET_STANDARD2_0 を追加する
+    //②　PlyFabUtil.cs の 53行目に || !NET_STANDARD2_0 を追加する
+
+}
